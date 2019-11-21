6a4067401cd9951da48007ca74b7a602bd80fcb4
avinash.yeccaluri@tyfone.com <avinash.yeccaluri@tyfone.com>
2019-11-21 10:01:26 +0530

    something

---
+++ CodeWars/DivisibleCount.java
@@ -0,0 +1,22 @@
+/**
+ * DivisibleCount
+ */
+public class DivisibleCount {
+
+    public static void main(String[] args) {
+        System.out.println(findNb(1071225));
+    }
+    public static long findNb(long m) {
+        long total = 0;
+    int counter = 0;
+    
+    while (total < m) {    
+      counter++;  
+      total = total + (long) Math.pow(counter, 3);            
+    }    
+    if (m == total)
+      return counter;
+    return -1;
+      } 
+
+}
---
+++ CodeWars/Test.java
@@ -0,0 +1,21 @@
+/**
+ * Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(findNb(1071225));
+        
+    }
+    public static long findNb(long m){
+        long total=0;
+        long counter=0;
+        while(total<m){
+            counter++;
+            total+=(long) Math.pow(counter, 3);
+        }
+        if(m== total){
+            return counter;
+        }
+        return -1;
+    }
+}
