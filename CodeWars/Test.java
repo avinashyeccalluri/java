5b4bdf6c0d13a9c3421bd055a5d5725f02e43c78
avinash.yeccaluri@tyfone.com <avinash.yeccaluri@tyfone.com>
2019-11-22 18:25:29 +0530

    22

--- CodeWars/Test.java
+++ CodeWars/Test.java
@@ -1,10 +1,21 @@
-/**
- * Test
- */
-/**
- * Test
- */
+import java.util.*;
 public class Test {
 
+    public static void main(String[] args) {
+        int[] a={0,1,3,6,10};
+        System.out.println(sumParts(a));
+    }
+    public static int[] sumParts(int[] ls) {
+        int sum=0;
+        int[] addedValues=new int[ls.length];
+        for(int i=0;i<ls.length;i++){
+            for(int j=i;j<ls.length;j++){
+                sum+=ls[j];
+            }
+            addedValues[i]=sum;
+        }
+
+        return null;
+    }
     
 }
