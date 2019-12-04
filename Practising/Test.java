0c0181c47a1d2a6bfcd083a1c731059f957ce122
avinash.yeccaluri@tyfone.com <avinash.yeccaluri@tyfone.com>
2019-12-04 11:43:59 +0530

    hello

--- Practising/Test.java
+++ Practising/Test.java
@@ -1,7 +1,7 @@
 /**
  * Test
  */
-import java.util.*;
+import java.util.*;     
 public class Test {
 
     public static void main(String[] args) {
---
+++ Practising/UnderSTanding.java
@@ -0,0 +1,12 @@
+/**
+ * UnderSTanding
+ */
+public class UnderSTanding {
+
+    public static void main(String[] args) {
+        underStandingReturn(2);
+    }
+    static void underStandingReturn(int a) {
+        return ;
+    }
+}
