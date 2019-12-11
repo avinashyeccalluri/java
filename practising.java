d986dbd390c9b0d2e0965f26f0dacca69d8dbfdd
avinash.yeccaluri@tyfone.com <avinash.yeccaluri@tyfone.com>
2019-12-11 18:49:32 +0530

    11/12

--- practising.java
+++ practising.java
@@ -1,15 +1,27 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * practising
  */
 public class practising {
 
   public static void main(String[] args) {
-    String a="a";
-    // System.out.println(Integer.parseInt(a));
-    try {
-      System.out.println(Integer.parseInt(a));
-    } catch (Exception e) {
-      System.out.println("hello");
-    }
+    Pattern p = Pattern.compile("[a-z]+|\\d+");
+        Matcher m = p.matcher("a1b22");
+        ArrayList<String> allMatches = new ArrayList<>();
+        while (m.find()) {
+        allMatches.add(m.group());
+        }
+        for (String string : allMatches) {
+          System.out.println(string);
+        }
+        // for(int i=1;i<allMatches.size();i+=2){
+        //     int k=i-1;
+        //     for(int j=0;j<Integer.parseInt(allMatches.get(i));j++){
+        //         System.out.print(allMatches.get(k));
+        //     }
+        // }
   }
 }
